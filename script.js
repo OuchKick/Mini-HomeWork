56ecbb7dd8171607140ab29418e8106d02200a68
Adam Reed <thirdeyeadam@gmail.com>
2021-03-31 11:08:47 -0500

    time ended

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 setInterval(function(){
 var today = moment();
 $("#welcomeHeader").text(today.format("MMM Do, YYYY, h:mm:ss a"));
-},1000);
+},1);
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
 
 
